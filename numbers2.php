e4ef5a3f7ebdbf3b7dda69126ce7958ce6570595
PolicjaZSI <createtomipl@gmail.com>
2022-10-05 10:53:13 +0200

    nowe

---
+++ numbers2.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+<body>
+<?php
+$x=5;
+$y=15;
+$a = $x + $y;
+echo "Wynik dodawania " .$x." i " .$y." wynosi " .$a."<br>";
+echo "<br>";
+$x=5;
+$y=15;
+$a = $x - $y;
+echo "Wynik odejmowania " .$x." i " .$y." wynosi " .$a."<br>";
+echo "<br>";
+$x=5;
+$y=15;
+$a = $x * $y;
+echo "Wynik mnożenia " .$x." i " .$y." wynosi " .$a."<br>";
+echo "<br>";
+$x=5;
+$y=15;
+$a = $x / $y;
+echo "Wynik dzielenia " .$x." i " .$y." wynosi " .$a."<br>";
+echo "<br>";
+$x=5;
+$y=15;
+$a = $x % $y;
+echo "Wynik dzielenia z reszta " .$x." i " .$y." wynosi " .$a."<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$x=4;
+$y=35;
+$a = $x * $y;
+echo "Pole prostokata o bokach  " .$x." i " .$y." wynosi " .$a. "j<sup>2</sup><br>";
+echo "<br>";
+$x=4;
+$y=35;
+$a = 2*$x + 2*$y;
+echo "Obwod prostokata " .$x." i " .$y." wynosi " .$a."<br>";
+echo "<br>";
+?>
+
+</body>
+</html>
