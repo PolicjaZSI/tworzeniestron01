245b8b25ab31a149e9c51f6c17057a812ae4b5df
PolicjaZSI <createtomipl@gmail.com>
2022-10-05 11:14:20 +0200

    noweret

---
+++ numbers3.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <style>
+       p {
+            color: aliceblue;
+
+            background-color: darkblue;
+
+        }
+    </style>
+</head>
+<body>
+<?php
+$a = 5
+
+?>
+
+<p><b>Wartosc funkcji abs z $a wynosi</b></p>
+
+</body>
+</html>
