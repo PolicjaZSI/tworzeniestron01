c65f0eb20ee070e81977875e7abc8a0514e13d5d
PolicjaZSI <createtomipl@gmail.com>
2022-10-26 09:42:45 +0200

    eeetre

---
+++ for.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+<style>
+    ol {
+        list-style-type: upper-roman;
+        margin-left: 40px;
+    }
+</style>
+<body>
+<ol>
+<?php
+for($n=1; $n<=100; $n++)
+{
+    echo "<li>to jest $n element listy<br>";
+}
+?>
+</ol>
+
+</body>
+</html>
