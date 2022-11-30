923c26087ed60e54de214d9aea68204c7febea2c
PolicjaZSI <createtomipl@gmail.com>
2022-11-30 09:23:24 +0100

    eeetre

--- conditionals.php
+++ conditionals.php
@@ -6,6 +6,7 @@
             text-align: center;
             color: darkmagenta;
         }
+
     </style>
 </head>
 <body>
@@ -13,12 +14,15 @@
 $losowa=rand(1,30);
 ?>
 <?php
-if ($losowa>=20); {
+if ($losowa >=20) {
     echo "wylosowana liczba jest wieksza lub rowna 20";
 }
-else
+else {
+    echo "wylosowana liczba jest mniejsza niz 20";
+}
 ?>
 
 <h1><?=$losowa ?></h1>
+<h2><?=if ?></h2>
 </body>
 </html>
