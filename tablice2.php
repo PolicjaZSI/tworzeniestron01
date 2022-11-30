ab40179d86388a4a0ea4dde3d5fdc99c5416ef91
PolicjaZSI <createtomipl@gmail.com>
2022-11-30 10:54:15 +0100

    eeetre

---
+++ tablice2.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+<?php
+$cos = array("Tworzenie", "stron", "aplikacji", "internetowych");?>
+
+<?php list($a, $b, $c, $d) = $cos;?>
+
+<?php echo "$a $b i $c $d.";?>
+
+
+</body>
+</html>
