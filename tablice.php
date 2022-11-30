93a83aae732d25d10ba16bd637911a4d04360b19
PolicjaZSI <createtomipl@gmail.com>
2022-11-30 10:04:09 +0100

    eeetre

---
+++ tablice.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+<pre>
+<?php
+        $owoce = array_fill(8, 5, "mango");
+        print_r($owoce);
+        ?>
+</pre>
+
+</body>
+</html>
