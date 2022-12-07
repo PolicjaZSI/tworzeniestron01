ab612484b12148d0102f276d417d061d0944c49b
PolicjaZSI <createtomipl@gmail.com>
2022-12-07 11:14:10 +0100

    eeetre

---
+++ asocjacyjna.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+<body>
+<pre>
+<?php
+$tytul = array("skazani na shawshank"=>"dramat","nietykalni"=>"biograficzny","wladca pierscieni powrot krola"=>"fantasy","pulp fiction"=>"gangsterski","siedem"=>"kryminał","podziemny krag"=>"thriller","django"=>"western","krol lew"=>"animacja","avengers wojna bez granic"=>"akcja","dobry,zly i brzydki"=>"western");
+
+print_r($tytul);
+?>
+    </pre>
+
+</body>
+</html>
